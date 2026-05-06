316f1fc18e6648328cb0695498d439203c167410
Lucas Wu <116535626+lucastiger@users.noreply.github.com>
2026-05-06 13:17:10 -0700

    Add delta_t_final to return dictionary in lle_solver

--- simulator/lle_solver.py
+++ simulator/lle_solver.py
@@ -228,7 +228,7 @@ def _single_trajectory_solver(
         xs=jnp.arange(t_slow),
         length=t_slow,
     )
-    _, _, e_snapshots, label_history, _ = final_carry
+    _, delta_t_final, e_snapshots, label_history, _ = final_carry
 
     return {
         "E_snapshots": e_snapshots,
@@ -237,6 +237,7 @@ def _single_trajectory_solver(
         "U_int_history": hist["U_int"],
         "DeltaT_history": hist["DeltaT"],
         "delta_omega_eff_history": hist["delta_omega_eff"],
+        "delta_t_final": delta_t_final,
     }
 
 
