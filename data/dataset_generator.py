2c8821a215d192549641215dcbb397155ae00e25
Lucas Wu <116535626+lucastiger@users.noreply.github.com>
2026-05-07 17:56:33 -0700

    Shuffle parameters in dataset generation
    Add random shuffling of parameters before dataset expansion and remove sorting after expansion.

--- data/dataset_generator.py
+++ data/dataset_generator.py
@@ -268,8 +268,12 @@ class DatasetGenerator:
     def generate_full_dataset(self, n_total: int = 45_000) -> None:
         param_list = list(self.full_simulation_list)
         repeats = int(math.ceil(n_total / max(1, len(param_list))))
+        
+        import random as _random
+        _rng = _random.Random(seed)
+        _rng.shuffle(param_list)        # in-place shuffle before repeating
+        
         expanded = (param_list * repeats)[:n_total]
-        expanded.sort(key=lambda x: (x["sweep_rate"], x["pin"], x["Gamma_th"]))
 
         out_path = self.output_dir / "dataset.h5"
         with h5py.File(out_path, "a") as h5file:
